45d14b0b5d5f5c7029a8ee4ebd75d5c0b4150996
Ihor Pohasii <aroundofyou@gmail.com>
2021-08-16 23:21:38 +0300

    Add Readme

---
+++ hello_berry_buns/README.md
@@ -0,0 +1,9 @@
+### Terminology
+
+*Oven* - An oven is a tool which is used to expose materials to a hot environment. Ovens contain a hollow chamber and provide a means of heating the chamber in a controlled way.
+
+*Table Spoon* (mentioned here as a Spoon) - a spoon larger than a teaspoon or a dessert spoon, used in serving food at the table and as a standard measuring unit in recipes.
+
+### Tips
+
+Process of melting butter - Melt butter on the stove if you want to end up with perfect, evenly melted butter, or if the recipe calls for browning. If you want to save time, use a microwave oven instead
--- hello_berry_buns/hello_berry_buns.cpp
+++ hello_berry_buns/hello_berry_buns.cpp
@@ -7,16 +7,16 @@ using namespace std;
 inline void keep_window_open() { char ch; cin >> ch; }
 
 int main() {
-  cout << "50g of butter dissolve not entirely\n";
+  cout << "50g of butter melt not entirely\n";
   cout << "Add 3 spoons of sugar\n";
   cout << "Add 15g of yeast\n";
   cout << "Leave to rest for 15 min\n";
-  cout << "Whip with fork 1 egg and bigger part of dissolved butter\n";
+  cout << "Whip with fork 1 egg and bigger part of melted butter\n";
   cout << "Pour in whipped egg with rest of butter and yeast\n";
   cout << "Mix and Knead the dough\n";
   cout << "Leave to rest for 1.5 hour\n";
   cout << "Roll out dough into layer 10mm thick\n";
-  cout << "Moisture with dissolved butter and 1 spoon of sugar\n";
+  cout << "Moisture the dough with melted butter and 1 spoon of sugar\n";
   cout << "Sprinkle with 200g of blueberries\n";
   cout << "Press the berries with rolling pin\n";
   cout << "Roll the dough into a roll\n";
@@ -24,7 +24,7 @@ int main() {
   cout << "Moisture baking sheet with vegetable oil\n";
   cout << "Put chunks on baking sheet, cover with a towel\n";
   cout << "Leave to rest for 20 min\n";
-  cout << "Bake in owen heaten up to 180 C\n";
+  cout << "Bake in oven heaten up to 180 C\n";
   keep_window_open();
   return 0;
 }
