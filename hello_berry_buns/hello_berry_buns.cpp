4da2d0cf271f1b86178018e96ee0e4f2764d347f
Ihor Pohasii <aroundofyou@gmail.com>
2021-08-16 23:07:22 +0300

    Add hello_berry_buns

---
+++ hello_berry_buns/hello_berry_buns.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+inline void keep_window_open() { char ch; cin >> ch; }
+
+int main() {
+  cout << "50g of butter dissolve not entirely\n";
+  cout << "Add 3 spoons of sugar\n";
+  cout << "Add 15g of yeast\n";
+  cout << "Leave to rest for 15 min\n";
+  cout << "Whip with fork 1 egg and bigger part of dissolved butter\n";
+  cout << "Pour in whipped egg with rest of butter and yeast\n";
+  cout << "Mix and Knead the dough\n";
+  cout << "Leave to rest for 1.5 hour\n";
+  cout << "Roll out dough into layer 10mm thick\n";
+  cout << "Moisture with dissolved butter and 1 spoon of sugar\n";
+  cout << "Sprinkle with 200g of blueberries\n";
+  cout << "Press the berries with rolling pin\n";
+  cout << "Roll the dough into a roll\n";
+  cout << "Cut roll into chunks 2cm thick\n";
+  cout << "Moisture baking sheet with vegetable oil\n";
+  cout << "Put chunks on baking sheet, cover with a towel\n";
+  cout << "Leave to rest for 20 min\n";
+  cout << "Bake in owen heaten up to 180 C\n";
+  keep_window_open();
+  return 0;
+}
