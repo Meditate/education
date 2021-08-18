2c9857992644df6b3acfe06868f78fb12c9bee55
Ihor Pohasii <aroundofyou@gmail.com>
2021-08-18 14:29:04 +0300

    Add measurement system

--- while/while_two.cpp
+++ while/while_two.cpp
@@ -5,12 +5,71 @@
 
 using namespace std;
 
+string symbolsFromString(string str) {
+  string clean_string = "";
+
+  for (int i = 0; i < str.length(); i++) {
+    if (!isdigit(str[i])) {
+      clean_string += str[i];
+    }
+  }
+
+  return clean_string;
+}
+
+double numbersFromString(string str) {
+  string clean_string = "";
+
+  for (int i = 0; i < str.length(); i++) {
+    if (isdigit(str[i])) {
+      clean_string += str[i];
+    }
+  }
+
+  return stod(clean_string);
+}
+
 int main() {
   vector<double> input;
+
+  double current_max = 0.0;
+  double current_min = 0.0;
+
   string temp;
- 
+
   while(cin >> temp && temp != "|") {
-    input.push_back(stod(temp));
+    string measurement = symbolsFromString(temp);
+    double input_value = numbersFromString(temp);
+
+    if( measurement == "cm" ) {}
+    else if(measurement == "in") {
+      input_value *= 2.54;
+    }
+    else if(measurement == "ft") {
+      input_value *= 12;
+      input_value *= 2.54;
+    }
+    else if(measurement == "m") {
+      input_value *= 100;
+    }
+    else {
+      cout << "ERROR in measurement system";
+    }
+
+    input.push_back(input_value);
+
+    if (input_value > current_max) {
+      current_max = input_value;
+
+      cout << "Biggest number \n";
+    }
+    else if (input_value < current_min) {
+      current_min = input_value;
+
+      cout << "Smallest number \n";
+    }
+
+    cout << temp << "\n";
 
     if(input.size() != 0 && input.size() % 2 == 0) {
       vector<double> output;
